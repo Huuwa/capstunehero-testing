e522162edebc8201a0bd2a6b598d71f702cbcade
Hassan <hasan@uxberts-MacBook-Pro.local>
2023-08-07 18:54:33 +0300

    app.py import packages

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
